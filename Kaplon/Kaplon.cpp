0c1fb6e1533a30cc85e0bf387528e95f73847811
JohnSnow46 <franekuna@gmail.com>
2023-03-13 13:27:23 +0100

    update

---
+++ Kaplon/Kaplon.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+#include <fstream>
+#include<windows.h>
+
+int loadingBar(int i) {
+
+    int counter = 0;
+    string loading[12];
+    loading[0] = "<";
+    loading[11] = ">";
+    
+    for (i = 1; i < 11; i++)
+    {
+        loading[i] = " ";
+    }
+
+    
+    for (int x = 0;x < 10;x++)
+    {
+        cout << "PROGRESS BAR: " << endl;
+        for (i = 0; i < 12; i++)
+        {
+            cout << loading[i];
+            
+        }
+        if (counter < 10)
+        loading[++counter] = "#";
+        Sleep(500);
+        system("CLS");
+    }
+    
+
+    return 0;
+}
+
+int main()
+{
+    loadingBar(10);
+}
