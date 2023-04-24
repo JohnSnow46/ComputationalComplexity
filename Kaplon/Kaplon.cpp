2826e26347ab032ad207962fc52f62d2445de7c6
JohnSnow46 <franekuna@gmail.com>
2023-04-24 19:53:53 +0200

    up

--- Kaplon/Kaplon.cpp
+++ Kaplon/Kaplon.cpp
@@ -16,14 +16,13 @@
 using namespace std;
 
 
-void loadingBar(int progress) {
-    const int barWidth = 50;
+void loadingBar(int progress, int barWidth = 50, char barChar = '#') {
     int pos = (barWidth * progress) / 100;
 
     cout << "PROGRESS BAR: [";
     for (int i = 0; i < barWidth; ++i) {
         if (i < pos) {
-            cout << "#";
+            cout << barChar;
         }
         else {
             cout << "-";
@@ -69,7 +68,7 @@ void saveTime(int howManyNumbers, int Time) {
 
 }
 
-void configIni(int* numberOfRepetitions, string* sortName)
+void configIni(int* numberOfRepetitions, string* sortName, int* numberOfInstances)
 {
     ifstream file("config.ini");
 
@@ -103,6 +102,9 @@ void configIni(int* numberOfRepetitions, string* sortName)
             }
             else if (key == "sortName") {
                 *sortName = value;
+            }
+            else if (key == "numberOfInstances") {
+                *numberOfInstances = stoi(value);
             }
         }
 
@@ -212,15 +214,67 @@ void quicksort(vector<int>& arr, int left, int right) {
     }
 }
 
+void countingSort(vector<int>& arr) {
+    int n = arr.size();
+    if (n == 0) {
+        return;
+    }
+
+    // Znajdź wartość maksymalną i minimalną
+    int max_val = arr[0], min_val = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max_val) {
+            max_val = arr[i];
+        }
+        if (arr[i] < min_val) {
+            min_val = arr[i];
+        }
+    }
+
+    // Zliczanie elementów
+    int k = max_val - min_val + 1;
+    vector<int> count(k, 0);
+    for (int i = 0; i < n; i++) {
+        count[arr[i] - min_val]++;
+    }
+
+    // Sumowanie ilości elementów mniejszych lub równych
+    for (int i = 1; i < k; i++) {
+        count[i] += count[i - 1];
+    }
+
+    // Tworzenie wynikowej tablicy
+    vector<int> result(n);
+    for (int i = n - 1; i >= 0; i--) {
+        result[count[arr[i] - min_val] - 1] = arr[i];
+        count[arr[i] - min_val]--;
+    }
+
+    // Zapisz wynik w tablicy oryginalnej
+    for (int i = 0; i < n; i++) {
+        arr[i] = result[i];
+    }
+}
 
 int main()
 {
     int numberOfRepetitions = 5;
     string sortName;
-    vector <int> instances = { 10, 20, 30, 40, 50, 75, 100, 150, 200, 350, 500, 750, 1000, 1000, 5000, 10000, 12000, 15000, 17000, 20000, 30000, 40000, 50000, 75000, 100000, 150000, 200000, 300000, 400000, 500000, 700000, 800000, 1000000 };
-    instances.reserve(instances.size());
+    int instanceNumber = 1;
+    vector <int> instancesSSS = { 10, 20, 30, 40, 50, 75, 100, 150, 200, 350, 500, 750, 1000, 1000, 5000, 10000, 12000, 15000, 17000, 20000, 30000, 40000, 50000, 75000, 100000, 150000, 200000, 300000, 400000, 500000, 700000, 800000, 1000000 };
 
-    configIni(&numberOfRepetitions, &sortName);
+    //cout << instancesSSS.size();
+
+    vector <int> instances;
+
+    configIni(&numberOfRepetitions, &sortName, &instanceNumber);
+
+    for (int i = 0; i < instanceNumber; i++) {
+        instances.push_back(instancesSSS[i]);
+        cout << instances[i] << " ";
+    }
+
+    cout << endl;
 
     vector <int> numbers;
     numbers.reserve(1000000);
@@ -255,6 +309,9 @@ int main()
             else if (sortName == "Bucket") {
                 bucketSort(copy);
             }
+            else if (sortName == "Counting") {
+                countingSort(copy);
+            }
             else {
                 cout << "Error: WRONG SORT NAME" << endl;
                 return 0;
@@ -271,9 +328,11 @@ int main()
             saveTime(instacesInLoop, time);
 
         }
-        loadingBar((float) x / sizeof(instances) * 100);
+        loadingBar((float) x / (instances.size() - 1) * 100);
 
     }
+
+    system("PAUSE");
 
     return 0;
 }
